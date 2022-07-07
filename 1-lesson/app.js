c7b85ed5d22bcb0100022ca1269f147e0d9775ca
User <user@MacBook-Air-User.local>
2022-07-07 16:17:33 +0600

    dz

---
+++ 1-lesson/app.js
@@ -0,0 +1,44 @@
+const a = "a";
+
+let c = 32;
+
+console.log(c)
+
+// c = "Hello!"
+//
+// console.log(c)
+
+const bool = true;
+const bool2 = false;
+
+const obj = {
+    key: 'value',
+    username:'aybek',
+    data: {
+        first_name: 'aybek',
+        last_name: 'rere',
+    }
+}
+// console.log(obj.data.last_name)
+
+const arr = ["Aidana", true,false,25]
+console.log(arr[0])
+
+const arr2 = [
+    {
+        key: 'value'
+    },
+    {
+        key:'value2'
+    },
+    {
+        key:'value3'
+    },
+]
+console.log(arr2[1].key)
+
+const name =  prompt( "Ты любишь анал?")
+// const log
+const age = alert("у тебя не побрито")
+
+// const first_number =prompt("первое число")
